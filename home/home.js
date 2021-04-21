70c4b4a51f8912b87d32bd7314cc44cfb3f58db5
kelianwei <keyan92@live.com>
2021-04-21 20:34:04 +0800

    整理421

--- home/home.js
+++ home/home.js
@@ -1,14 +1,18 @@
 var toggleul = {
     handleEvent(event) {
         if (event.type != "click") { return; }
-        event.target.nextElementSibling.hidden = !event.target.nextElementSibling.hidden;
+        var ele = event.target.nextElementSibling ? event.target.nextElementSibling : event.target.parentNode.nextElementSibling;
+        ele.hidden = !ele.hidden;
     }
 }
 document.addEventListener("DOMContentLoaded", function () {
     for (let elem of document.querySelectorAll("div.index>span")) {
         elem.nextElementSibling.hidden = true;
+        elem.insertAdjacentHTML("beforeend", "   <small class='count'>(" + elem.nextElementSibling.children.length + "个属性)</small>")
         elem.addEventListener("click", toggleul);
     }
+    document.querySelector(".count").textContent = document.querySelector(".index").querySelectorAll("li").length
+        ;
 });
 
 
